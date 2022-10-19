d786269349f41ba5d73b0c682fba1ef5c21cf315
MaryChiodelli <marychiodelli97@gmail.com>
2022-10-19 16:29:40 +0200

    add CRUD index in post

--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,5 @@ Route::middleware('auth')
 ->namespace('Admin')
 ->group(function () {
     Route::get('/home', 'HomeController@index')->name('home');
+    Route::resource('posts', 'PostController');
 });
